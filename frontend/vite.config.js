d6351fd5b17b823edf2a8f3f72e92b08f03f7d19
Nailah Aulia R <nailahcans18@gmail.com>
2026-07-04 21:19:56 +0700

    perbaikan

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // Mengarahkan semua request yang diawali dengan '/api' ke server backend
+      '/api': {
+        target: 'http://localhost:8000', // ⚠️ GANTI dengan URL & Port Backend kamu (misal port Laravel 8000, atau Express 5000)
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
